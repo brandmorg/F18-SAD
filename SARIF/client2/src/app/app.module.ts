869d40843d98c6ff6dc921b472109fc003d32521
assesspool <assesspool@gmail.com>
2018-10-24 18:04:00 -0400

    Server test base
    Removed user-details
    Changed part of user-component aesthetic
    Temp removal of modal component conflicting with binding
    Ready for "ng build --prod"

--- SARIF/client2/src/app/app.module.ts
+++ SARIF/client2/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { AddUserComponent } from './add-user/add-user.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
+//import { UserDetailsComponent } from './user-details/user-details.component';
 import { LoginHomeComponent } from './login-home/login-home.component';
 import { ChartOfAccountsComponent } from './chart-of-accounts/chart-of-accounts.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
@@ -26,7 +26,7 @@ import { JournalizeComponent } from './journalize/journalize.component';
     LoginComponent,
     UserPageComponent,
     AddUserComponent,
-    UserDetailsComponent,
+   // UserDetailsComponent,
     LoginHomeComponent,
     ChartOfAccountsComponent,
     ResetPasswordComponent,
