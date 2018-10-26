81e54d4a8995c269654cba2869efcaeed9486f4b
wgambrell <tylergam@yahoo.com>
2018-10-25 21:28:34 -0400

    Accounts now arranged by correct order on trial balance

--- SARIF/client2/src/app/trial-balance/trial-balance.component.ts
+++ SARIF/client2/src/app/trial-balance/trial-balance.component.ts
@@ -9,8 +9,15 @@ import {CoA} from '../chart-of-accounts';
 })
 export class TrialBalanceComponent implements OnInit {
   accounts = [];
+  accountsArranged = [];
   debitTotal = 0;
   creditTotal = 0;
+
+  assetslist = [];
+  liabilitiesList = [];
+  equitList = [];
+  revenueList = [];
+  expensesList = [];
 
   constructor(
     private cserv: CoAService,
@@ -25,14 +32,54 @@ export class TrialBalanceComponent implements OnInit {
   }
 
   async viewAccounts() {
+    //get list of chart of accounts
     let result = await this.cserv.findAllSort('caId','ASC', 'All', null).toPromise();
     this.accounts = result;
+    //arranging accounts by type
+    for(let acc of this.accounts){
+      if(acc.accountType == 'Assets'){
+        this.assetslist.push(acc);
+      }
+      else if(acc.accountType == 'Liability'){
+        this.liabilitiesList.push(acc);
+      }
+      else if(acc.accountType == 'Equity'){
+        this.equitList.push(acc);
+      }
+      else if(acc.accountType == 'Revenue'){
+        this.revenueList.push(acc);
+      }
+      else{
+        this.expensesList.push(acc);
+      }
+    }
+    //put arranged lists into one list
+
+    for(let asset of this.assetslist){
+      this.accountsArranged.push(asset);
+    }
+    for(let liability of this.liabilitiesList){
+      this.accountsArranged.push(liability);
+    }
+    for(let equity of this.equitList){
+      this.accountsArranged.push(equity);
+    }
+    for(let revenue of this.revenueList){
+      this.accountsArranged.push(revenue);
+    }
+    for(let expense of this.expensesList){
+      this.accountsArranged.push(expense);
+    }
+
+
+
+
     this.totalDebit();
     this.totalCredit();
   }
 
   totalDebit(){
-    for(let acc of this.accounts){
+    for(let acc of this.accountsArranged){
       console.log(acc.currentBalance);
       if(acc.normalSide == 'Debit'){
         this.debitTotal = +this.debitTotal + +acc.currentBalance;
@@ -40,7 +87,7 @@ export class TrialBalanceComponent implements OnInit {
     }
   }
   totalCredit(){
-    for(let acc of this.accounts){
+    for(let acc of this.accountsArranged){
       console.log(acc.currentBalance);
       if(acc.normalSide == 'Credit'){
         this.creditTotal = +this.creditTotal + +acc.currentBalance;
